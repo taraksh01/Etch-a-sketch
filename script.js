85c77af1ea590a116e0022cf4ea7fa0850f28b0d
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-13 18:54:13 +0530

    Created a 16X16 grid using javascript and styled using css

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("I am working fine");
+const container = document.querySelector("#container");
+
+for (let i = 0; i < 16; i++) {
+  for (let j = 0; j < 16; j++) {
+    const child = document.createElement("div");
+    child.classList.add("child");
+    container.appendChild(child);
+  }
+}
+
