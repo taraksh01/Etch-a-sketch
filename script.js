781c33e4c6d7c1d494cfed3bc720afb4514881a1
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-06-13 19:34:56 +0530

    Added event listener to black button that when clicked set the value of gridColor to black

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ clear.addEventListener("click", () => {
   gridColor = "whitesmoke";
 });
 
+black.addEventListener("click", () => {
+  gridColor = "black";
+});
+
 function createGrid() {
   for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
